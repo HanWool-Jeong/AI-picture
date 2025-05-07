41e87dd309fdc681891d8f7b114713fc5e5aed30
hanwool <soink366739@gmail.com>
2025-05-07 14:07:47 +0900

    프롬프트 파파고 번역기능 제거

--- server/router/text2img_router.js
+++ server/router/text2img_router.js
@@ -22,48 +22,30 @@ router
 
         print_log(req.ip, `generation 요청 (prompt: ${prompt})`);
 
-        // prompt 한글->영어로 번역하기
-        papago_translate(prompt)
-            .then(res => {
-                if (res.status == 200) {
-                    return res.json();
-                } else {
-                    throw(new Error("파파고 번역 오류"));
-                }
-            })
-            .then(translated => {
-                print_log(req.ip, "generation 파파고 번역 완료");
+        make_generation(prompt)
+        .then(result => {
+            // ai가 생성한 사진 클라에게 보내기
+            print_log(req.ip, 'generation 요청 전송');
+            
+            const image_name = `${Date.now()}.png`;
+            const image_path = project_dir + '/generations/' + image_name;
+            res.send({ url: result, name: image_name });
 
-                make_generation(translated.message.result.translatedText)
-                    .then(result => {
-                        // ai가 생성한 사진 클라에게 보내기
-                        print_log(req.ip, 'generation 요청 전송');
-                        
-                        const image_name = `${Date.now()}.png`;
-                        const image_path = project_dir + '/generations/' + image_name;
-                        res.send({ url: result, name: image_name });
-
-                        // ai가 생성한 사진 다운로드
-                        print_log(req.ip, 'generation 사진 업로드 중');
-                        download_image_from_url(result, image_path)
-                            .then(result => {
-                                print_log(req.ip, 'generation 사진 업로드 성공');
-                            })
-                            .catch(err => {
-                                print_log(req.ip, 'generation 사진 업로드 실패: ' + err.message);
-                                next(err);
-                            });
-                    })
-                    .catch(err => {
-                        print_log(req.ip, 'generation 실패: ' + err.message);
-                        next(err);
-                    });
+            // ai가 생성한 사진 다운로드
+            print_log(req.ip, 'generation 사진 업로드 중');
+            download_image_from_url(result, image_path)
+            .then(result => {
+                print_log(req.ip, 'generation 사진 업로드 성공');
             })
             .catch(err => {
-                print_log(req.ip, 'generation 파파고 오류: ' + err.message);
+                print_log(req.ip, 'generation 사진 업로드 실패: ' + err.message);
                 next(err);
             });
-
+        })
+        .catch(err => {
+            print_log(req.ip, 'generation 실패: ' + err.message);
+            next(err);
+        });
     });
 
 export default router;
